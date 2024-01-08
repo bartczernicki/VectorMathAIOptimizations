195b9dd1d4871608cb20b0b333e6cc27a6253261
Bart Czernicki <bartczernicki@outlook.com>
2024-01-07 23:15:02 -0500

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,25 +1,19 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-using System.Collections.Concurrent;
-using System.Numerics.Tensors;
+﻿using BenchmarkDotNet.Running;
 using System.Runtime.Intrinsics;
-using Microsoft.SemanticKernel;
 
 
 namespace VectorEmbeddingsSimilarityOptimizations
 {
-    [MemoryDiagnoser(true)]
-    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.SlowestToFastest)]
-    [SimpleJob(runStrategy: RunStrategy.Throughput, runtimeMoniker: RuntimeMoniker.Net80)]
-    [Config(typeof(VectorEmbeddingsSimilarityOptimizations.Util.BenchmarkConfig))]
+    //[MemoryDiagnoser(true)]
+    //[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.SlowestToFastest)]
+    //[SimpleJob(runStrategy: RunStrategy.Throughput, runtimeMoniker: RuntimeMoniker.Net80)]
+    //[Config(typeof(VectorEmbeddingsSimilarityOptimizations.Util.BenchmarkConfig))]
     public class Program
     {
         private Util.Vectors? vectors;
 
         // Processor Count (set at 75% in code)
-        private static int ProcessorsAvailableAt75Percent = Util.BenchmarkConfig.ProcessorsAvailableAt75Percent;
+        //private static int ProcessorsAvailableAt75Percent = Util.BenchmarkConfig.ProcessorsAvailableAt75Percent;
 
         static void Main(string[] args)
         {
@@ -32,52 +26,52 @@ namespace VectorEmbeddingsSimilarityOptimizations
         #endif
 
             //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
-            var summary = BenchmarkRunner.Run<Program>();
+            var summary = BenchmarkRunner.Run<Jobs.VectorCalculation.Benchmark>();
             // print results
             Console.WriteLine(summary.ToString());
         }
 
-        [GlobalSetup]
-        public void Setup()
-        {
-            // Generate float vectors that match dimension size of Open-Source and OpenAI Embeddings
-            // MTEB Database: https://huggingface.co/spaces/mteb/leaderboard 
-            // 768 Dimensions is a popular embeddings dimension size for several leaderboard open-source models
-            // 1536 Dimensions is the size of the ada-002 model for OpenAI/Azure OpenAI embeddings
-            this.vectors = new VectorEmbeddingsSimilarityOptimizations.Util.Vectors(NumberOfVectorsToCreate);
+        //[GlobalSetup]
+        //public void Setup()
+        //{
+        //    // Generate float vectors that match dimension size of Open-Source and OpenAI Embeddings
+        //    // MTEB Database: https://huggingface.co/spaces/mteb/leaderboard 
+        //    // 768 Dimensions is a popular embeddings dimension size for several leaderboard open-source models
+        //    // 1536 Dimensions is the size of the ada-002 model for OpenAI/Azure OpenAI embeddings
+        //    this.vectors = new VectorEmbeddingsSimilarityOptimizations.Util.Vectors(NumberOfVectorsToCreate);
 
-            ProcessorsAvailableAt75Percent = (int)(0.75 * Environment.ProcessorCount);
-        }
+        //    ProcessorsAvailableAt75Percent = (int)(0.75 * Environment.ProcessorCount);
+        //}
 
-        [Params(1000, 100)] //<-- Change this to determine the amount of vectors to "mimic" a Vector database  (very small, large)
-        // 1mil embeddings is roughly 700,000-1mil document pages with a decent amount of text present
-        public int NumberOfVectorsToCreate { get; set; }
+        //[Params(1000, 100)] //<-- Change this to determine the amount of vectors to "mimic" a Vector database  (very small, large)
+        //// 1mil embeddings is roughly 700,000-1mil document pages with a decent amount of text present
+        //public int NumberOfVectorsToCreate { get; set; }
 
-        [Params(false, true)] //<-- Change this to determine if to run on a single thread or leverage 75% of available threads 
-        public bool MultiThreaded { get; set; }
+        //[Params(false, true)] //<-- Change this to determine if to run on a single thread or leverage 75% of available threads 
+        //public bool MultiThreaded { get; set; }
 
-        [Benchmark]
-        public void CosineSimilarityVectors768Dimensions()
-        {
-            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo768Dimensions, vectors?.TestVectors768Dimensions, true, MultiThreaded);
-        }
+        //[Benchmark]
+        //public void CosineSimilarityVectors768Dimensions()
+        //{
+        //    var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo768Dimensions, vectors?.TestVectors768Dimensions, true, MultiThreaded);
+        //}
 
-        [Benchmark]
-        public void DotProductVectors768Dimensions()
-        {
-            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo768Dimensions, vectors?.TestVectors768Dimensions, false, MultiThreaded);
-        }
+        //[Benchmark]
+        //public void DotProductVectors768Dimensions()
+        //{
+        //    var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo768Dimensions, vectors?.TestVectors768Dimensions, false, MultiThreaded);
+        //}
 
-        [Benchmark]
-        public void CosineSimilarityVectors1536Dimensions()
-        {
-            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, true, MultiThreaded);
-        }
+        //[Benchmark]
+        //public void CosineSimilarityVectors1536Dimensions()
+        //{
+        //    var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, true, MultiThreaded);
+        //}
 
-        [Benchmark]
-        public void DotProductVectors1536Dimensions()
-        {
-            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, false, MultiThreaded);
-        }
+        //[Benchmark]
+        //public void DotProductVectors1536Dimensions()
+        //{
+        //    var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, false, MultiThreaded);
+        //}
     }
 }
---
+++ VectorEmbeddingsSimilarityOptimizations.Jobs.VectorCalculation/Benchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+
+namespace VectorEmbeddingsSimilarityOptimizations.Jobs.VectorCalculation
+{
+    [MemoryDiagnoser(true)]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.SlowestToFastest)]
+    [SimpleJob(runStrategy: RunStrategy.Throughput, runtimeMoniker: RuntimeMoniker.Net80)]
+    [Config(typeof(Util.BenchmarkConfig))]
+    public class Benchmark
+    {
+        private Util.Vectors? vectors;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.vectors = new Util.Vectors(NumberOfVectorsToCreate);
+        }
+
+        [Params(1000)] //<-- Change this to determine the amount of vectors to "mimic" a Vector database  (very small, large)
+        // 1mil embeddings is roughly 700,000-1mil document pages with a decent amount of text present
+        public int NumberOfVectorsToCreate { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public void CosineSimilarityVectors1536Dimensions()
+        {
+            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, true, false);
+        }
+
+        [Benchmark]
+        public void DotProductVectors1536Dimensions()
+        {
+            var results = Util.Vectors.TopMatchingVectors(vectors?.VectorToCompareTo1536Dimensions, vectors?.TestVectors1536Dimensions, false, false);
+        }
+    }
+}
